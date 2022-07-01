4183663a15715df34fe337fc477722c8f629d71b
yuki_maitani <fightzoi1997@gmail.com>
2022-07-01 23:19:36 +0900

    正しく数字が表示されないバグを修正

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,21 +11,24 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
+    var isFinishedTypingNumber = true
     
     
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
-        
-        //What should happen when a non-number button is pressed
-    
+        isFinishedTypingNumber = true
     }
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         if let numValue = sender.currentTitle {
-            displayLabel.text! += numValue
+            if isFinishedTypingNumber == true {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
         }
     }
 
 }
-
